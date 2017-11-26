de6f7f8da9b20852aa67d346e6cadfc62eb61988
Jaga Santagostino <jagasantagostino@gmail.com>
2017-11-26 23:00:00 +0100

    remove unsed code

--- handler.js
+++ handler.js
@@ -1,44 +1,34 @@
-'use strict';
+'use strict'
 const http = require('http')
 const url = require('url')
-const ioProfiler = require('lambda-profiler')({})
-
-var signRequest = (requestOptions) => { return requestOptions }
-if (process.env.AWS_SIGN_REQUESTS) {
-  signRequest = require('aws4').sign
-}
-
-var profiler = (func) => { return func }
-if (process.env.TRACE_REQUESTS) {
-  profiler = ioProfiler
-}
 
 const httpAgent = http.Agent({
   keepAlive: true,
-  keepAliveMsecs: 300000
+  keepAliveMsecs: 300000,
 })
 
-module.exports.proxy = profiler((event, context, callback) => {
-  var res = http.request(signRequest({
+module.exports.proxy = (event, context, callback) => {
+  var res = http.request({
     hostname: process.env.URL,
     method: event.httpMethod,
     headers: event.headers,
     path: event.path,
     agent: httpAgent,
-    timeout: 30 /* maximum time for api gateway invoked Lambda */
-  }))
+    timeout: 30 /* maximum time for api gateway invoked Lambda */,
+  })
 
-  var resBody = ""
-  res.on('data', (chunk) => {
-    resBody += chunk
-  })
-  .on('end', () => {
-    var response = {
-      statusCode: res.statusCode,
-      headers: res.headers,
-      body: resBody
-    }
-    console.log(response)
-    callback(null, response)
-  })
-})
+  var resBody = ''
+  res
+    .on('data', chunk => {
+      resBody += chunk
+    })
+    .on('end', () => {
+      var response = {
+        statusCode: res.statusCode,
+        headers: res.headers,
+        body: resBody,
+      }
+      console.log(response)
+      callback(null, response)
+    })
+}
